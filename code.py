f5bcd87821d25f584035f31fd5c77987656e712b
Tony Wu <82834362+twu425@users.noreply.github.com>
2025-09-15 06:17:54 -0700

    create custom_hid class

--- code.py
+++ code.py
@@ -36,57 +36,9 @@ i2c1 = busio.I2C(scl=board.GP1, sda=board.GP0, frequency=100000)
 i2c2 = busio.I2C(scl=board.GP7, sda=board.GP6, frequency=100000) 
 i2c3 = bitbangio.I2C(scl=board.GP9, sda=board.GP8, frequency=100000) # The pi pico only has 2 hardware i2c busses, so a third one is bit-banged onto GPIO 8 and 9
 
-rotation_sensor_1 = AS5600(i2c2) # Arm 1 rotation sensor
-rotation_sensor_2 = AS5600(i2c3) # Arm 2 rotation sensor
-rotation_sensor_3 = AS5600(i2c1) # Turntable rotation sensor
-
-def determine_pos(l1, l2, offset, rotation_1, rotation_2, rotation_3):
-    """
-    :param l1: Length of arm 1 (mm)
-    :param l2: Length of arm 2 (mm)
-    :param rotation_1: Rotation of arm 1 (radians)
-    :param rotation_2: Rotation of arm 2 (radians)
-    :param rotation_3: Turntable rotation (radians)
-    :return: A tuple containing the x, y, and z coordinates of the endpoint
-    """
-        
-    # Determine the position of the start of the first arm
-    x1 = (math.sin(rotation_1) * l1) + offset
-    y1 = 0
-    z1 = math.cos(rotation_1) * l1
-    #print(x1, y1, z1)
-    
-    # Determine the position of the start of the second arm 
-    x2 = x1 + math.sin(rotation_2 + rotation_1) * l2
-    y2 = y1 
-    z2 = z1 + math.cos(rotation_2 + rotation_1) * l2
-    #print(x2, y2, z2)
-    
-    # Determine the position of the pen-point
-    x3 = (x2 * math.cos(rotation_3)) - (y2 * math.sin(rotation_3))
-    y3 = (x2 * math.sin(rotation_3)) - (y2 * math.cos(rotation_3))
-    z3 = z2
-    
-    return (x3, y3, z3)
-    
-    
-def determine_height(l1, l2, rotation_1, rotation_2):
-    """
-    :param l1: Length of arm 1
-    :param l2: Length of arm 2
-    :param rotation_1: Rotation of arm 1
-    :param rotation_2: Rotation of arm 2
-    :return: Z-position of the end point
-    """
-    rotation_1 = math.radians(rotation_1)
-    rotation_2 = math.radians(rotation_2)
-
-
-    z = (math.cos(rotation_1) * l1) + (math.cos(rotation_1 + rotation_2) * l2)
-
-    return z
-
-
+rotation1_sensor = AS5600(i2c2) # Arm 1 rotation sensor
+rotation2_sensor = AS5600(i2c3) # Arm 2 rotation sensor
+rotation3_sensor = AS5600(i2c1) # Turntable rotation sensor
 
 
 # All measures are in millimeters
@@ -94,50 +46,22 @@ arm1_length = 170
 arm2_length = 205
 offset = 0
 
-# Coordinates
-x = 0
-y = 0
-z = 0
-
-# Previous coordinates
-previous_x = x
-previous_y = y
-previous_z = z
-
-# Accumulation
-# TODO: Explain this
-accumulation_x = 0
-accumulation_y = 0
-accumulation_z = 0
-
-# Move
-move_x = 0
-move_y = 0
-move_z = 0
-
 sensitivity = 10
 mouse_smoothing_count = 3 # Determines how many of the last N mouse coordinates to average out for smoother movement
 threshold = 2.0  # Ignore movements less than this
-
-arm1_rotation_moving_average = MovingAverage(size=mouse_smoothing_count)
-arm2_rotation_moving_average = MovingAverage(size=mouse_smoothing_count)
-turntable_rotation_moving_average = MovingAverage(size=mouse_smoothing_count)
 
 # Offsets
 arm1_rotation_offset = 360-286 # Arm 1
 arm2_rotation_offset = 360-111 # Arm 2
 turntable_rotation_offset = 360-0 # Turntable
-# def callibrate(arm1_raw_rotation, arm2_raw_rotation, turntable_raw_rotation):
-#     arm1_rotation_offset = (2 * math.pi) - arm1_raw_rotation
-#     arm2_rotation_offset = (2 * math.pi) - arm2_raw_rotation
-#     turntable_rotation_offset = (2 * math.pi) - turntable_raw_rotation
+
 ##############
 ## Buttons
 ##############
 
-m1_pin = board.GP11
-m2_pin = board.GP12
-m3_pin = board.GP13
+button1_pin = board.GP11
+button2_pin = board.GP12
+button3_pin = board.GP13
 
 def registerButton(button_pin):
     button = digitalio.DigitalInOut(button_pin)
@@ -145,63 +69,17 @@ def registerButton(button_pin):
     button.pull = digitalio.Pull.UP
     return button
 
-m1_btn = registerButton(m1_pin)
-m2_btn = registerButton(m2_pin)
-m3_btn = registerButton(m3_pin)
+button1 = registerButton(button1_pin)
+button2 = registerButton(button2_pin)
+button3 = registerButton(button3_pin)
 
 ##########
 ## Loop
 ##########
-
+from custom_hid import CustomHid
+device = CustomHid(mouse, custom, arm1_length, arm2_length, 
+                   rotation1_sensor, rotation2_sensor, rotation3_sensor,
+                   button1, button2, button3)
 while True:
-        
-    arm1_raw_rotation = ((rotation_sensor_1.angle / 4096) * 2 * math.pi + math.radians(arm1_rotation_offset)) % (2 * math.pi)
-    arm2_raw_rotation = ((rotation_sensor_2.angle / 4096) * 2 * math.pi + math.radians(arm2_rotation_offset)) % (2 * math.pi)
-    arm3_raw_rotation = ((rotation_sensor_3.angle / 4096) * 2 * math.pi + math.radians(turntable_rotation_offset)) % (2 * math.pi)
-    
-    arm1_rotation = arm1_rotation_moving_average.add(arm1_raw_rotation)
-    arm2_rotation = arm2_rotation_moving_average.add(arm2_raw_rotation)
-    arm3_rotation = turntable_rotation_moving_average.add(arm3_raw_rotation)
-    
-    #print("r1 " + str(arm1_rotation))
-    #print("r2 " + str(arm2_rotation))
-    #print("r3 " + str(arm3_rotation))
-
-    x, y, z = determine_pos(arm1_length, arm2_length, offset, arm1_rotation, arm2_rotation, arm3_rotation)
-
-    accumulation_x += (x - previous_x) * sensitivity
-    accumulation_y += (y - previous_y) * sensitivity
-    accumulation_z += (y - previous_z) * sensitivity
-
-    previous_x = x
-    previous_y = y
-    previous_z = z
-    
-    move_x = int(accumulation_x)
-    move_y = int(accumulation_y)
-    move_z = int(accumulation_z)
-    
-    # TODO: implement threshold to take into accound total distance
-    if abs(accumulation_x) >= threshold or abs(accumulation_y) >= threshold:
-        move_x = int(accumulation_x)
-        move_y = int(accumulation_y)
-        mouse.move(move_x, move_y)
-        accumulation_x -= move_x
-        accumulation_y -= move_y
-    
-    # Avoid spamming HID reports, as it slows down the microcontroller immensely 
-    if not m1_btn.value:  
-        mouse.press(Mouse.LEFT_BUTTON)
-    else:
-        mouse.release(Mouse.LEFT_BUTTON)
-    if not m2_btn.value:  
-        mouse.press(Mouse.RIGHT_BUTTON)
-    else:
-        mouse.release(Mouse.RIGHT_BUTTON)
-        
-    if not m3_btn.value:  
-        mouse.press(Mouse.RIGHT_BUTTON)
-    else:
-        mouse.release(Mouse.RIGHT_BUTTON)    
-        
-    #time.sleep(0.005)
+    # device.callibrate()
+    device.update()
--- custom_hid.py
+++ custom_hid.py
@@ -1,2 +1,161 @@
+from moving_average import MovingAverage
+import math
+
+
 class CustomHid:
-    def __init__
+
+    def __init__(self, mouse, custom_hid,
+                 arm1_length, arm2_length, 
+                 rotation_sensor_1, rotation_sensor_2, rotation_sensor_3, 
+                 button_1, button_2, button_3,
+                 sensitivity = 10, mouse_smoothing_count = 3, threshold = 2,
+                 profile = 0):
+        self.mouse = mouse
+        self.custom_hid = custom_hid
+
+        self.arm1_length = arm1_length
+        self.arm2_length = arm2_length
+
+        self.rotation_sensor_1 = rotation_sensor_1
+        self.rotation_sensor_2 = rotation_sensor_2
+        self.rotation_sensor_3 = rotation_sensor_3
+        self.button_1 = button_1
+        self.button_2 = button_2
+        self.button_3 = button_3
+
+        self.sensitivity = sensitivity
+        self.mouse_smoothing_count = mouse_smoothing_count
+        self.threshold = threshold
+        self.profile = profile
+
+        self.arm1_rotation_moving_average = MovingAverage(size=mouse_smoothing_count)
+        self.arm2_rotation_moving_average = MovingAverage(size=mouse_smoothing_count)
+        self.turntable_rotation_moving_average = MovingAverage(size=mouse_smoothing_count)
+
+        self.arm1_rotation_offset = 360-286 # Arm 1
+        self.arm2_rotation_offset = 360-111 # Arm 2
+        self.turntable_rotation_offset = 360-0 # Turntable
+
+        # Previous coordinates
+        self.previous_x = 0
+        self.previous_y = 0
+        self.previous_z = 0
+
+        # Accumulation
+        # TODO: Explain this
+        self.accumulation_x = 0
+        self.accumulation_y = 0
+        self.accumulation_z = 0
+
+        # Move
+        self.move_x = 0
+        self.move_y = 0
+        self.move_z = 0
+
+        self.arm1_rotation_offset = 0
+        self.arm2_rotation_offset = 0
+        self.turntable_rotation_offset = 0
+
+    def get_rotations(self):
+        arm1_raw_rotation = ((self.rotation_sensor_1.angle / 4096) * 2 * math.pi + math.radians(self.arm1_rotation_offset)) % (2 * math.pi)
+        arm2_raw_rotation = ((self.rotation_sensor_2.angle / 4096) * 2 * math.pi + math.radians(self.arm2_rotation_offset)) % (2 * math.pi)
+        arm3_raw_rotation = ((self.rotation_sensor_3.angle / 4096) * 2 * math.pi + math.radians(self.turntable_rotation_offset)) % (2 * math.pi)
+        
+        arm1_rotation = self.arm1_rotation_moving_average.add(arm1_raw_rotation)
+        arm2_rotation = self.arm2_rotation_moving_average.add(arm2_raw_rotation)
+        arm3_rotation = self.turntable_rotation_moving_average.add(arm3_raw_rotation)
+
+        return arm1_rotation, arm2_rotation, arm3_rotation
+        
+    def determine_pos(self):
+        """
+        :param l1: Length of arm 1 (mm)
+        :param l2: Length of arm 2 (mm)
+        :param rotation_1: Rotation of arm 1 (radians)
+        :param rotation_2: Rotation of arm 2 (radians)
+        :param rotation_3: Turntable rotation (radians)
+        :return: A tuple containing the x, y, and z coordinates of the endpoint
+        """
+        rotation_1, rotation_2, rotation_3 = self.get_rotations()
+            
+        # Determine the position of the start of the first arm
+        x1 = (math.sin(rotation_1) * self.arm1_length) #+ offset
+        y1 = 0
+        z1 = math.cos(rotation_1) * self.arm1_length
+        #print(x1, y1, z1)
+        
+        # Determine the position of the start of the second arm 
+        x2 = x1 + math.sin(rotation_2 + rotation_1) * self.arm1_length
+        y2 = y1 
+        z2 = z1 + math.cos(rotation_2 + rotation_1) * self.arm1_length
+        #print(x2, y2, z2)
+        
+        # Determine the position of the pen-point
+        x3 = (x2 * math.cos(rotation_3)) - (y2 * math.sin(rotation_3))
+        y3 = (x2 * math.sin(rotation_3)) - (y2 * math.cos(rotation_3))
+        z3 = z2
+        
+        return (x3, y3, z3)
+    
+    def callibrate(self):
+        arm1_rotation, arm2_rotation, turntable_rotation = self.get_rotations()
+        self.arm1_rotation_offset = (2 * math.pi) - arm1_rotation
+        self.arm2_rotation_offset = (2 * math.pi) - arm2_rotation
+        self.turntable_rotation_offset = (2 * math.pi) - turntable_rotation
+        
+    def update(self):
+        x, y, z = self.determine_pos()
+        self.accumulation_x += (x - self.previous_x) * self.sensitivity
+        self.accumulation_y += (y - self.previous_y) * self.sensitivity
+        self.accumulation_z += (z - self.previous_z) * self.sensitivity
+        threshold = 0
+
+        self.previous_x = x
+        self.previous_y = y
+        self.previous_z = z
+    
+        self.move_x = int(self.accumulation_x)
+        self.move_y = int(self.accumulation_y)
+        self.move_z = int(self.accumulation_z)
+        
+        # TODO: implement threshold to take into account total distance
+        if abs(self.accumulation_x) >= threshold or abs(self.accumulation_y) >= threshold:
+            self.move_x = int(self.accumulation_x)
+            self.move_y = int(self.accumulation_y)
+            self.mouse.move(self.move_x, self.move_y)
+            self.accumulation_x -= self.move_x
+            self.accumulation_y -= self.move_y
+        
+        # Avoid spamming HID reports, as it slows down the microcontroller immensely 
+        # if not m1_btn.value:  
+        #     self.mouse.press(Mouse.LEFT_BUTTON)
+        # else:
+        #     mouse.release(Mouse.LEFT_BUTTON)
+        # if not m2_btn.value:  
+        #     mouse.press(Mouse.RIGHT_BUTTON)
+        # else:
+        #     mouse.release(Mouse.RIGHT_BUTTON)
+            
+        # if not m3_btn.value:  
+        #     mouse.press(Mouse.RIGHT_BUTTON)
+        # else:
+        #     mouse.release(Mouse.RIGHT_BUTTON)    
+
+
+
+
+    # def determine_height(l1, l2, rotation_1, rotation_2):
+    #     """
+    #     :param l1: Length of arm 1
+    #     :param l2: Length of arm 2
+    #     :param rotation_1: Rotation of arm 1
+    #     :param rotation_2: Rotation of arm 2
+    #     :return: Z-position of the end point
+    #     """
+    #     rotation_1 = math.radians(rotation_1)
+    #     rotation_2 = math.radians(rotation_2)
+
+
+    #     z = (math.cos(rotation_1) * l1) + (math.cos(rotation_1 + rotation_2) * l2)
+
+    #     return z
